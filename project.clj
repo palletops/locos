f087ca22b37d809bee598258538b1842f032a38d
Hugo Duncan <hugo@hugoduncan.org>
2012-11-26 21:36:19 -0500

    Fix groupid for core.logic dependency

--- project.clj
+++ project.clj
@@ -2,5 +2,5 @@
   :description "Logic based Configuration Functions"
   :url "https://github.com/palletops/locos"
   :license {:name "All rights reserved."}
-  :dependencies [[core.logic "0.8.0-beta3-SNAPSHOT"]
+  :dependencies [[org.clojure/core.logic "0.8.0-beta3-SNAPSHOT"]
                  [org.clojure/clojure "1.4.0"]])
