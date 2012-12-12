cd6223e5083e28f38e454add12c8f51d5b303877
Hugo Duncan <hugo@hugoduncan.org>
2012-12-12 15:41:49 -0500

    Update to core.logic 0.8.0-beta3 release

--- project.clj
+++ project.clj
@@ -2,6 +2,6 @@
   :description "Logic based Configuration Functions"
   :url "https://github.com/palletops/locos"
   :license {:name "All rights reserved."}
-  :dependencies [[org.clojure/core.logic "0.8.0-beta3-SNAPSHOT"]
+  :dependencies [[org.clojure/core.logic "0.8.0-beta3"]
                  [org.clojure/tools.logging "0.1.2"]
                  [org.clojure/clojure "1.4.0"]])
